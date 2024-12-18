fab6cbe03ee829c6ae3f70f75029c1b43601a673
Thalinhos <thalissonvieira@hotmail.com.br>
2024-12-18 12:44:50 -0300

    foi

--- backend/main.ts
+++ backend/main.ts
@@ -6,7 +6,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://os-metroplan.deno.dev',
+}));
 
 app.use(express.static('public'))
 
