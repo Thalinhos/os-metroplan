6c41ad831476e483dcb507b8e95e292ba16e50a5
Thalinhos <thalissonvieira@hotmail.com.br>
2024-12-18 12:35:08 -0300

    dist -> public

--- backend/main.ts
+++ backend/main.ts
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static('dist'))
+app.use(express.static('public'))
 
 app.post('/api/ordemdeservico', async (req, res) => {
 
